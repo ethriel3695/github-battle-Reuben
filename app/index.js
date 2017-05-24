51398912ec5a1037e78942416810a2b88ffac887
GOVERNET\reubenellis <rellis@governet.net>
2017-05-24 14:15:45 -0600

    Added package information and a component

--- app/index.js
+++ app/index.js
@@ -1,17 +1,18 @@
 var React = require('react');
-var React = require('react-dom');
+var ReactDOM = require('react-dom');
 require('./index.css');
-
-//state
-//lifecycle event
-//UI
 
 class App extends React.Component {
     render () {
         return (
             <div>
-                Hello World!
+                Hello React Training!
             </div>
         )
     }
 }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
